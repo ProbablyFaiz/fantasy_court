082e3c7ee0a67590b06dc961c34d4af6a9026c02
Faiz Surani <faiz.surani@gmail.com>
2025-10-19 20:21:35 -0700

    Remove test job schedule

--- backend/court/jobs/schedule.py
+++ backend/court/jobs/schedule.py
@@ -1,10 +1,6 @@
 from celery.schedules import crontab
 
 BEAT_SCHEDULE = {
-    "test-job": {
-        "task": "court.jobs.tasks.test_job",
-        "schedule": crontab(minute="*/1"),
-    },
     "fantasy-court-pipeline": {
         "task": "court.jobs.tasks.run_fantasy_court_pipeline",
         "schedule": crontab(minute="*/30"),  # Run every 30 minutes
